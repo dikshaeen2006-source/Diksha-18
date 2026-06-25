10747e760151a775c6298705b8d87082a1b08d58
dikshaeen2006-source <dikshaeen2006@gmail.com>
2026-06-25 22:36:30 +0530

    Add Signup.js with initial styling and layout

---
+++ Signup.js
@@ -0,0 +1,205 @@
+*{
+    margin: 0;
+    padding: 0;
+    font-family: regular;
+}
+
+@font-face {
+    font-family: title;
+    src: url('./font/title.woff2');
+}
+
+@font-face {
+    font-family: regular;
+    src: url('./font/regular.woff2');
+}
+
+body{
+    background: #121212;
+}
+
+.container{
+    width: 380px;
+    padding: 0px 28px;
+    margin: auto;
+    text-align: center;
+}
+
+.logo{
+    width: 80px;
+    padding: 32px 0px 24px;
+    margin: auto;
+    overflow: hidden;
+}
+
+.logo img{
+    width: 100%;
+}
+
+h1{
+    color: white;
+    font-family: title;
+    font-size: 48px;
+}
+
+span{
+    display: block;
+}
+
+form{
+    padding: 20px;
+    text-align: left;
+    margin-top: 16px;
+}
+
+label{
+    font-family: 16px;
+    color: white;
+}
+
+input{
+    width: 100%;
+    padding: 12px 0px;
+    padding-left: 8px;
+    margin: 8px 0px;
+    border-radius: 6px;
+    background: none;
+    border: 1px solid #cccccc;
+    color: white;
+}
+
+input:hover{
+    border: 1px solid white;
+}
+
+form a{
+    color: #1ed760;
+    font-size: 14px;
+}
+
+.next{
+    all: unset;
+    width: 320px;
+    padding: 12px;
+    margin: 20px 0px;
+    background: #1ed760;
+    color: #121212;
+    font-weight: 600;
+    text-align: center;
+    border-radius: 32px;
+    cursor: pointer;
+}
+
+.line{
+    width: 100%;
+    height: 0.3px;
+    margin: 16px 0px;
+    text-align: center;
+    background: #cccccc;
+    position: relative;
+}
+
+.line::before{
+    content: "or";
+    width: 40px;
+    height: 40px;
+    position: absolute;
+    transform: translate(-50%,-50%);
+    left: 50%;
+    top: 100%;
+    background: #121212;
+    color: white;
+    border-radius: 50%;
+    line-height: 40px;
+}
+
+.icon-btn{
+    all: unset;
+    width: 320px;
+    padding: 12px;
+    margin: 4px 0px;
+    border: 1px solid #cccccc;
+    color: white;
+    font-family: title;
+    font-weight: 600;
+    text-align: center;
+    border-radius: 32px;
+    cursor: pointer;
+    overflow: hidden;
+}
+
+.icon-btn img{
+    width: 20px;
+    float: left;
+    margin-left: 12px;
+}
+
+.icon-btn:hover{
+    border: 1px solid white;
+}
+
+.line2{
+    width: 100%;
+    height: 0.3px;
+    margin: 28px 0px;
+    text-align: center;
+    background: #cccccc;
+    opacity: 0.5;
+}
+
+.already{
+    text-align: center;
+    color: #cccccc;
+}
+
+.login-here{
+    color: white;
+    font-size: 16px;
+}
+
+.term{
+    margin-top: 24px;
+    font-size: 12px;
+    color: #cccccc;
+    text-align: center;
+}
+
+.privacy{
+    color: #cccccc;
+    font-size: 12px;
+}
+
+.home{
+    all: unset;
+    width: 320px;
+    padding: 12px;
+    margin: 4px 0px;
+    border: 1px solid #cccccc;
+    color: white;
+    font-family: title;
+    font-weight: 600;
+    text-align: center;
+    border-radius: 32px;
+    cursor: pointer;
+    overflow: hidden;
+}
+/* ===== Search Box ===== */
+.searchBox {
+  width: 100%;
+  padding: 10px 20px;
+}
+
+.searchBox input {
+  width: 100%;
+  padding: 10px 15px;
+  border-radius: 20px;
+  border: none;
+  outline: none;
+  font-size: 15px;
+  background: #2a2a2a;
+  color: #fff;
+}
+
+.searchBox input::placeholder {
+  color: #aaa;
+}
